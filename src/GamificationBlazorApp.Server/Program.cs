acafc8020f805ab7be80dcbdcc0a789b86f112b4
goncalo.amador <goncalo.amador@symbioticsgroup.com>
2025-08-29 19:53:11 +0100

    Base Gamification Blazor App structure

---
+++ src/GamificationBlazorApp.Server/Program.cs
@@ -0,0 +1,24 @@
+//var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddControllers();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//// InMemory DB
+//builder.Services.AddDbContext<GamificationDbContext>(options =>
+//    options.UseInMemoryDatabase("GamificationDb"));
+
+//// Add services
+//builder.Services.AddScoped<ApplicationService>();
+//// ... other services
+
+//var app = builder.Build();
+
+//app.UseSwagger();
+//app.UseSwaggerUI();
+
+//app.UseHttpsRedirection();
+//app.UseAuthorization();
+//app.MapControllers();
+
+//app.Run();
